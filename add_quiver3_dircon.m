881a8525c8f5d131a672fd86b571d6777af21b85
shiganai <shiganai.hiro@gmail.com>
2021-08-14 00:32:45 +0900

    xdir installation on add-quiver3

--- add_quiver3_dircon.m
+++ add_quiver3_dircon.m
@@ -1,4 +1,31 @@
-function add_quiver3_dircon(towards,base)
+function add_quiver3_dircon(position, xdir, ydir, zdir)
+
+if xdir > 0
+    towards_x = 1 + xdir : size(position, 1);
+    base_x = 1 : size(position, 1) - xdir;
+else
+    base_x = 1 - xdir : size(position, 1);
+    towards_x = 1 : size(position, 1) + xdir;
+end
+
+if ydir > 0
+    towards_y = 1 + ydir : size(position, 2);
+    base_y = 1 : size(position, 2) - ydir;
+else
+    base_y = 1 - ydir : size(position, 2);
+    towards_y = 1 : size(position, 2) + ydir;
+end
+
+if zdir > 0
+    towards_z = 1 + zdir : size(position, 3);
+    base_z = 1 : size(position, 3) - zdir;
+else
+    base_z = 1 - zdir : size(position, 3);
+    towards_z = 1 : size(position, 3) + zdir;
+end
+
+towards = position(towards_x, towards_y, towards_z, :);
+base = position(base_x, base_y, base_z, :);
 
 target_direction = towards - base;
 
