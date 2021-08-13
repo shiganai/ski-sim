03ff566d22831e13ade41e06c40902d0046d86b3
shiganai <shiganai.hiro@gmail.com>
2021-08-14 00:50:25 +0900

    Update board_from_dots.m
    add xyz force
    add external force
    modify for sization

--- board_from_dots.m
+++ board_from_dots.m
@@ -1,6 +1,8 @@
 
 %% initialize
 
+clear all
+
 col_num = 5;
 row_num = 5;
 stair_num = 3;
@@ -11,9 +13,9 @@ c = 5;
 position_init = NaN(row_num, col_num, stair_num, 3);
 velocity_init = zeros(size(position_init));
 
-position_init(:, :, :, 1) = ones(1, row_num, stair_num) .* (1:row_num)';
-position_init(:, :, :, 2) = ones(col_num, 1, stair_num) .* (1:col_num);
-matrix_tmp(1, 1, 1:stair_num) = 1:stair_num; matrix_tmp = matrix_tmp * 1;
+position_init(:, :, :, 1) = ones(1, col_num, stair_num) .* (1:row_num)';
+position_init(:, :, :, 2) = ones(row_num, 1, stair_num) .* (1:col_num);
+matrix_tmp(1, 1, 1:stair_num) = 1:stair_num; matrix_tmp = matrix_tmp * 0.5;
 position_init(:, :, :, 3) = ones(row_num, col_num, stair_num) .* matrix_tmp;
 
 %% straight forces
@@ -51,28 +53,45 @@ zx_length_init = vecnorm(zx_direction_init, 2, 4);
 xz_direction_init = calc_dir_and_force(position_init, -1, 0, 1);
 xz_length_init = vecnorm(xz_direction_init, 2, 4);
 
+%% xyz forces
+
+xyz_direction_init = calc_dir_and_force(position_init, 1, 1, 1);
+xyz_length_init = vecnorm(xyz_direction_init, 2, 4);
+
+yxz_direction_init = calc_dir_and_force(position_init, 1, -1, 1);
+yxz_length_init = vecnorm(yxz_direction_init, 2, 4);
+
+zxy_direction_init = calc_dir_and_force(position_init, -1, 1, 1);
+zxy_length_init = vecnorm(zxy_direction_init, 2, 4);
+
+zyx_direction_init = calc_dir_and_force(position_init, -1, -1, 1);
+zyx_length_init = vecnorm(zyx_direction_init, 2, 4);
 
 %% at times
 
 % position = position_init + rand(size(position)) * 0.3;
 position = position_init;
-position(:, :, :, 3) = position(:, :, :, 3) + ones(1, row_num, stair_num) .* (((1:row_num)'-3)/2).^2;
+position(:, :, :, 3) = position(:, :, :, 3) + ones(1, col_num, stair_num) .* (((1:row_num)'-3)/2).^2;
 
 velocity = velocity_init;
 
 x_tmp = position(:, :, :, 1); x_tmp = x_tmp(:);
 y_tmp = position(:, :, :, 2); y_tmp = y_tmp(:);
 z_tmp = position(:, :, :, 3); z_tmp = z_tmp(:);
-scatp = scatter3(x_tmp, y_tmp, z_tmp);
+scatp = scatter3(x_tmp, y_tmp, z_tmp, 2);
 xlabel('X')
 ylabel('Y')
 zlabel('Z')
-xlim([0, 6])
-ylim([0, 6])
-zlim([0, 6])
+xlim([0, row_num + 1])
+ylim([0, col_num + 1])
+zlim([0, stair_num + 1])
 daspect(ones(1,3))
 
-time = 0:1e-2:3; time = time';
+time = 0:1e-2:10; time = time';
+
+    
+external_force = zeros(size(position));
+external_force(1,1,1,3) = 10;
 
 for time_index = 1:size(time, 1)
     
@@ -100,7 +119,16 @@ for time_index = 1:size(time, 1)
     [~, force] = calc_dir_and_force(position, 1, 0, 1, zx_length_init, force, k);
     [~, force] = calc_dir_and_force(position, -1, 0, 1, xz_length_init, force, k);
     
-    force = force - c * velocity;
+    %% xyz forces
+    
+    [~, force] = calc_dir_and_force(position, 1, 1, 1, xyz_length_init, force, k);
+    [~, force] = calc_dir_and_force(position, 1, -1, 1, yxz_length_init, force, k);
+    [~, force] = calc_dir_and_force(position, -1, 1, 1, zxy_length_init, force, k);
+    [~, force] = calc_dir_and_force(position, -1, -1, 1, zyx_length_init, force, k);
+    
+    %%
+    
+    force = force - c * velocity + external_force;
     
     position = position + velocity .* diff(time(1:2));
     velocity = velocity + force .* diff(time(1:2));
@@ -111,7 +139,10 @@ for time_index = 1:size(time, 1)
     scatp.XData = x_tmp;
     scatp.YData = y_tmp;
     scatp.ZData = z_tmp;
-    drawnow
+    
+    if time(time_index) < 3
+        drawnow
+    end
     
 end
 
