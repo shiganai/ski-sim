a52fcc13020c7fde61d879b27113abdaa8a5deed
shiganai <shiganai.hiro@gmail.com>
2021-08-14 00:15:38 +0900

    Update board_from_dots.m
    enable negative input

--- calc_dir_and_force.m
+++ calc_dir_and_force.m
@@ -1,12 +1,37 @@
 function [direction, force] = calc_dir_and_force(position, xdir, ydir, zdir, length_init, force, k)
 
-direction = position(1+xdir:end, 1+ydir:end, 1+zdir:end, :) - position(1:end-xdir, 1:end-ydir, 1:end-zdir, :);
+if xdir > 0
+    towards_x = 1 + xdir : size(position, 1);
+    base_x = 1 : size(position, 1) - xdir;
+else
+    base_x = 1 - xdir : size(position, 1);
+    towards_x = 1 : size(position, 1) + xdir;
+end
+
+if ydir > 0
+    towards_y = 1 + ydir : size(position, 2);
+    base_y = 1 : size(position, 2) - ydir;
+else
+    base_y = 1 - ydir : size(position, 2);
+    towards_y = 1 : size(position, 2) + ydir;
+end
+
+if zdir > 0
+    towards_z = 1 + zdir : size(position, 3);
+    base_z = 1 : size(position, 3) - zdir;
+else
+    base_z = 1 - zdir : size(position, 3);
+    towards_z = 1 : size(position, 3) + zdir;
+end
+
+direction = position(towards_x, towards_y, towards_z, :) ...
+    - position(base_x, base_y, base_z, :);
 
 if nargin >= 5 && nargout >= 2
     length = vecnorm(direction, 2, 4);
     force_tmp = -k * direction .* (length - length_init);
-    force(1+xdir:end, 1+ydir:end, 1+zdir:end, :) = force(1+xdir:end, 1+ydir:end, 1+zdir:end, :) + force_tmp;
-    force(1:end-xdir, 1:end-ydir, 1:end-zdir, :) = force(1:end-xdir, 1:end-ydir, 1:end-zdir, :) - force_tmp;
+    force(towards_x, towards_y, towards_z, :) = force(towards_x, towards_y, towards_z, :) + force_tmp;
+    force(base_x, base_y, base_z, :) = force(base_x, base_y, base_z, :) - force_tmp;
 end
 
 end
