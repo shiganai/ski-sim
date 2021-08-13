9bcf2c9189f0613873f78b07a3020b1376826215
shiganai <shiganai.hiro@gmail.com>
2021-08-13 22:53:35 +0900

    Update board_from_dots.m
    add euler integral

--- board_from_dots.m
+++ board_from_dots.m
@@ -1,141 +1,155 @@
 
+%% initialize
+
 col_num = 5;
 row_num = 5;
 stair_num = 3;
+k = 1;
+m = 1;
 
-matrix_position_initial = NaN(row_num, col_num, stair_num, 3);
+position_init = NaN(row_num, col_num, stair_num, 3);
+velocity_init = zeros(size(position_init));
 
-matrix_position_initial(:, :, :, 1) = ones(1, row_num, stair_num) .* (1:row_num)';
-matrix_position_initial(:, :, :, 2) = ones(col_num, 1, stair_num) .* (1:col_num);
-matrix_tmp(1, 1, 1:stair_num) = 1:stair_num; matrix_tmp = matrix_tmp * 3;
-matrix_position_initial(:, :, :, 3) = ones(row_num, col_num, stair_num) .* matrix_tmp;
+position_init(:, :, :, 1) = ones(1, row_num, stair_num) .* (1:row_num)';
+position_init(:, :, :, 2) = ones(col_num, 1, stair_num) .* (1:col_num);
+matrix_tmp(1, 1, 1:stair_num) = 1:stair_num; matrix_tmp = matrix_tmp * 1;
+position_init(:, :, :, 3) = ones(row_num, col_num, stair_num) .* matrix_tmp;
 
-matrix_position = matrix_position_initial;
-x_tmp = matrix_position(:, :, :, 1); x_tmp = x_tmp(:);
-y_tmp = matrix_position(:, :, :, 2); y_tmp = y_tmp(:);
-z_tmp = matrix_position(:, :, :, 3); z_tmp = z_tmp(:);
-scatter3(x_tmp, y_tmp, z_tmp)
+%% straight forces
+
+x_direction_init = position_init(2:end, :, :, :) - position_init(1:end-1, :, :, :);
+x_length_init = vecnorm(x_direction_init, 2, 4);
+
+y_direction_init = position_init(:, 2:end, :, :) - position_init(:, 1:end-1, :, :);
+y_length_init = vecnorm(y_direction_init, 2, 4);
+
+z_direction_init = position_init(:, :, 2:end, :) - position_init(:, :, 1:end-1, :);
+z_length_init = vecnorm(z_direction_init, 2, 4);
+
+%% xy forces
+
+xy_direction_init = position_init(2:end, 2:end, :, :) - position_init(1:end-1, 1:end-1, :, :);
+xy_length_init = vecnorm(xy_direction_init, 2, 4);
+
+yx_direction_init = position_init(2:end, 1:end-1, :, :) - position_init(1:end-1, 2:end, :, :);
+yx_length_init = vecnorm(yx_direction_init, 2, 4);
+
+%% yz forces
+
+yz_direction_init = position_init(:, 2:end, 2:end, :) - position_init(:, 1:end-1, 1:end-1, :);
+yz_length_init = vecnorm(yz_direction_init, 2, 4);
+
+zy_direction_init = position_init(:, 2:end, 1:end-1, :) - position_init(:, 1:end-1, 2:end, :);
+zy_length_init = vecnorm(zy_direction_init, 2, 4);
+
+%% zx forces
+
+zx_direction_init = position_init(2:end, :, 2:end, :) - position_init(1:end-1, :, 1:end-1, :);
+zx_length_init = vecnorm(zx_direction_init, 2, 4);
+
+xz_direction_init = position_init(1:end-1, :, 2:end, :) - position_init(2:end, :, 1:end-1, :);
+xz_length_init = vecnorm(xz_direction_init, 2, 4);
+
+
+%% at times
+
+position = position_init;
+position(1, 1, 1, 1) = 0;
+position(1, 1, 1, 2) = 0;
+position(1, 1, 1, 3) = 0;
+
+velocity = velocity_init;
+
+x_tmp = position(:, :, :, 1); x_tmp = x_tmp(:);
+y_tmp = position(:, :, :, 2); y_tmp = y_tmp(:);
+z_tmp = position(:, :, :, 3); z_tmp = z_tmp(:);
+scatp = scatter3(x_tmp, y_tmp, z_tmp);
 xlabel('X')
 ylabel('Y')
 zlabel('Z')
 daspect(ones(1,3))
 
-%% straight forces
+time = 0:1e-2:1; time = time';
 
-matrix_x_direction = matrix_position(2:end, :, :, :) - matrix_position(1:end-1, :, :, :);
-
-x_tmp = matrix_position(1:end-1, :, :, 1); x_tmp = x_tmp(:);
-y_tmp = matrix_position(1:end-1, :, :, 2); y_tmp = y_tmp(:);
-z_tmp = matrix_position(1:end-1, :, :, 3); z_tmp = z_tmp(:);
-u_tmp = matrix_x_direction(:, :, :, 1); u_tmp = u_tmp(:);
-v_tmp = matrix_x_direction(:, :, :, 2); v_tmp = v_tmp(:);
-w_tmp = matrix_x_direction(:, :, :, 3); w_tmp = w_tmp(:);
-hold on
-quiver3(x_tmp, y_tmp, z_tmp, u_tmp, v_tmp, w_tmp)
-hold off
-
-matrix_y_direction = matrix_position(:, 2:end, :, :) - matrix_position(:, 1:end-1, :, :);
-
-x_tmp = matrix_position(:, 1:end-1, :, 1); x_tmp = x_tmp(:);
-y_tmp = matrix_position(:, 1:end-1, :, 2); y_tmp = y_tmp(:);
-z_tmp = matrix_position(:, 1:end-1, :, 3); z_tmp = z_tmp(:);
-u_tmp = matrix_y_direction(:, :, :, 1); u_tmp = u_tmp(:);
-v_tmp = matrix_y_direction(:, :, :, 2); v_tmp = v_tmp(:);
-w_tmp = matrix_y_direction(:, :, :, 3); w_tmp = w_tmp(:);
-hold on
-quiver3(x_tmp, y_tmp, z_tmp, u_tmp, v_tmp, w_tmp)
-hold off
-
-matrix_z_direction = matrix_position(:, :, 2:end, :) - matrix_position(:, :, 1:end-1, :);
-
-x_tmp = matrix_position(:, :, 1:end-1, 1); x_tmp = x_tmp(:);
-y_tmp = matrix_position(:, :, 1:end-1, 2); y_tmp = y_tmp(:);
-z_tmp = matrix_position(:, :, 1:end-1, 3); z_tmp = z_tmp(:);
-u_tmp = matrix_z_direction(:, :, :, 1); u_tmp = u_tmp(:);
-v_tmp = matrix_z_direction(:, :, :, 2); v_tmp = v_tmp(:);
-w_tmp = matrix_z_direction(:, :, :, 3); w_tmp = w_tmp(:);
-hold on
-quiver3(x_tmp, y_tmp, z_tmp, u_tmp, v_tmp, w_tmp)
-hold off
-
-%% xy forces
-
-matrix_xy_direction = matrix_position(2:end, 2:end, :, :) - matrix_position(1:end-1, 1:end-1, :, :);
-
-x_tmp = matrix_position(1:end-1, 1:end-1, :, 1); x_tmp = x_tmp(:);
-y_tmp = matrix_position(1:end-1, 1:end-1, :, 2); y_tmp = y_tmp(:);
-z_tmp = matrix_position(1:end-1, 1:end-1, :, 3); z_tmp = z_tmp(:);
-u_tmp = matrix_xy_direction(:, :, :, 1); u_tmp = u_tmp(:);
-v_tmp = matrix_xy_direction(:, :, :, 2); v_tmp = v_tmp(:);
-w_tmp = matrix_xy_direction(:, :, :, 3); w_tmp = w_tmp(:);
-hold on
-quiver3(x_tmp, y_tmp, z_tmp, u_tmp, v_tmp, w_tmp)
-hold off
-
-matrix_yx_direction = matrix_position(2:end, 1:end-1, :, :) - matrix_position(1:end-1, 2:end, :, :);
-
-x_tmp = matrix_position(1:end-1, 2:end, :, 1); x_tmp = x_tmp(:);
-y_tmp = matrix_position(1:end-1, 2:end, :, 2); y_tmp = y_tmp(:);
-z_tmp = matrix_position(1:end-1, 2:end, :, 3); z_tmp = z_tmp(:);
-u_tmp = matrix_yx_direction(:, :, :, 1); u_tmp = u_tmp(:);
-v_tmp = matrix_yx_direction(:, :, :, 2); v_tmp = v_tmp(:);
-w_tmp = matrix_yx_direction(:, :, :, 3); w_tmp = w_tmp(:);
-hold on
-quiver3(x_tmp, y_tmp, z_tmp, u_tmp, v_tmp, w_tmp)
-hold off
-
-%% yz forces
-
-matrix_yz_direction = matrix_position(:, 2:end, 2:end, :) - matrix_position(:, 1:end-1, 1:end-1, :);
-
-x_tmp = matrix_position(:, 1:end-1, 1:end-1, 1); x_tmp = x_tmp(:);
-y_tmp = matrix_position(:, 1:end-1, 1:end-1, 2); y_tmp = y_tmp(:);
-z_tmp = matrix_position(:, 1:end-1, 1:end-1, 3); z_tmp = z_tmp(:);
-u_tmp = matrix_yz_direction(:, :, :, 1); u_tmp = u_tmp(:);
-v_tmp = matrix_yz_direction(:, :, :, 2); v_tmp = v_tmp(:);
-w_tmp = matrix_yz_direction(:, :, :, 3); w_tmp = w_tmp(:);
-hold on
-quiver3(x_tmp, y_tmp, z_tmp, u_tmp, v_tmp, w_tmp)
-hold off
-
-matrix_zy_direction = matrix_position(:, 2:end, 1:end-1, :) - matrix_position(:, 1:end-1, 2:end, :);
-
-x_tmp = matrix_position(:, 1:end-1, 2:end, 1); x_tmp = x_tmp(:);
-y_tmp = matrix_position(:, 1:end-1, 2:end, 2); y_tmp = y_tmp(:);
-z_tmp = matrix_position(:, 1:end-1, 2:end, 3); z_tmp = z_tmp(:);
-u_tmp = matrix_zy_direction(:, :, :, 1); u_tmp = u_tmp(:);
-v_tmp = matrix_zy_direction(:, :, :, 2); v_tmp = v_tmp(:);
-w_tmp = matrix_zy_direction(:, :, :, 3); w_tmp = w_tmp(:);
-hold on
-quiver3(x_tmp, y_tmp, z_tmp, u_tmp, v_tmp, w_tmp)
-hold off
-
-%% zx forces
-
-matrix_zx_direction = matrix_position(2:end, :, 2:end, :) - matrix_position(1:end-1, :, 1:end-1, :);
-
-x_tmp = matrix_position(1:end-1, :, 1:end-1, 1); x_tmp = x_tmp(:);
-y_tmp = matrix_position(1:end-1, :, 1:end-1, 2); y_tmp = y_tmp(:);
-z_tmp = matrix_position(1:end-1, :, 1:end-1, 3); z_tmp = z_tmp(:);
-u_tmp = matrix_zx_direction(:, :, :, 1); u_tmp = u_tmp(:);
-v_tmp = matrix_zx_direction(:, :, :, 2); v_tmp = v_tmp(:);
-w_tmp = matrix_zx_direction(:, :, :, 3); w_tmp = w_tmp(:);
-hold on
-quiver3(x_tmp, y_tmp, z_tmp, u_tmp, v_tmp, w_tmp)
-hold off
-
-matrix_xz_direction = matrix_position(1:end-1, :, 2:end, :) - matrix_position(2:end, :, 1:end-1, :);
-
-x_tmp = matrix_position(2:end, :, 1:end-1, 1); x_tmp = x_tmp(:);
-y_tmp = matrix_position(2:end, :, 1:end-1, 2); y_tmp = y_tmp(:);
-z_tmp = matrix_position(2:end, :, 1:end-1, 3); z_tmp = z_tmp(:);
-u_tmp = matrix_xz_direction(:, :, :, 1); u_tmp = u_tmp(:);
-v_tmp = matrix_xz_direction(:, :, :, 2); v_tmp = v_tmp(:);
-w_tmp = matrix_xz_direction(:, :, :, 3); w_tmp = w_tmp(:);
-hold on
-quiver3(x_tmp, y_tmp, z_tmp, u_tmp, v_tmp, w_tmp)
-hold off
-
+for time_index = 1:size(time, 1)
+    
+    
+    force = zeros(size(position));
+    
+    %% straight forces
+    
+    x_direction = position(2:end, :, :, :) - position(1:end-1, :, :, :);
+    x_length = vecnorm(x_direction, 2, 4);
+    x_force = -k * x_direction .* (x_length - x_length_init);
+    force(2:end, :, :, :) = force(2:end, :, :, :) + x_force;
+    force(1:end-1, :, :, :) = force(1:end-1, :, :, :) - x_force;
+    
+    y_direction = position(:, 2:end, :, :) - position(:, 1:end-1, :, :);
+    y_length = vecnorm(y_direction, 2, 4);
+    y_force = -k * y_direction .* (y_length - y_length_init);
+    force(:, 2:end, :, :) = force(:, 2:end, :, :) + y_force;
+    force(:, 1:end-1, :, :) = force(:, 1:end-1, :, :) - y_force;
+    
+    z_direction = position(:, :, 2:end, :) - position(:, :, 1:end-1, :);
+    z_length = vecnorm(z_direction, 2, 4);
+    z_force = -k * z_direction .* (z_length - z_length_init);
+    force(:, :, 2:end, :) = force(:, :, 2:end, :) + z_force;
+    force(:, :, 1:end-1, :) = force(:, :, 1:end-1, :) - z_force;
+    
+    %% xy forces
+    
+    xy_direction = position(2:end, 2:end, :, :) - position(1:end-1, 1:end-1, :, :);
+    xy_length = vecnorm(xy_direction, 2, 4);
+    xy_force = -k * xy_direction .* (xy_length - xy_length_init);
+    force(2:end, 2:end, :, :) = force(2:end, 2:end, :, :) + xy_force;
+    force(1:end-1, 1:end-1, :, :) = force(1:end-1, 1:end-1, :, :) - xy_force;
+    
+    yx_direction = position(2:end, 1:end-1, :, :) - position(1:end-1, 2:end, :, :);
+    yx_length = vecnorm(yx_direction, 2, 4);
+    yx_force = -k * yx_direction .* (yx_length - yx_length_init);
+    force(2:end, 1:end-1, :, :) = force(2:end, 1:end-1, :, :) + yx_force;
+    force(1:end-1, 2:end, :, :) = force(1:end-1, 2:end, :, :) - yx_force;
+    
+    %% yz forces
+    
+    yz_direction = position(:, 2:end, 2:end, :) - position(:, 1:end-1, 1:end-1, :);
+    yz_length = vecnorm(yz_direction, 2, 4);
+    yz_force = -k * yz_direction .* (yz_length - yz_length_init);
+    force(:, 2:end, 2:end, :) = force(:, 2:end, 2:end, :) + yz_force;
+    force(:, 1:end-1, 1:end-1, :) = force(:, 1:end-1, 1:end-1, :) - yz_force;
+    
+    zy_direction = position(:, 2:end, 1:end-1, :) - position(:, 1:end-1, 2:end, :);
+    zy_length = vecnorm(zy_direction, 2, 4);
+    zy_force = -k * zy_direction .* (zy_length - zy_length_init);
+    force(:, 2:end, 1:end-1, :) = force(:, 2:end, 1:end-1, :) + zy_force;
+    force(:, 1:end-1, 2:end, :) = force(:, 1:end-1, 2:end, :) - zy_force;
+    
+    %% zx forces
+    
+    zx_direction = position(2:end, :, 2:end, :) - position(1:end-1, :, 1:end-1, :);
+    zx_length = vecnorm(zx_direction, 2, 4);
+    zx_force = -k * zx_direction .* (zx_length - zx_length_init);
+    force(2:end, :, 2:end, :) = force(2:end, :, 2:end, :) + zx_force;
+    force(1:end-1, :, 1:end-1, :) = force(1:end-1, :, 1:end-1, :) - zx_force;
+    
+    xz_direction = position(1:end-1, :, 2:end, :) - position(2:end, :, 1:end-1, :);
+    xz_length = vecnorm(xz_direction, 2, 4);
+    xz_force = -k * xz_direction .* (xz_length - xz_length_init);
+    force(1:end-1, :, 2:end, :) = force(1:end-1, :, 2:end, :) + xz_force;
+    force(2:end, :, 1:end-1, :) = force(2:end, :, 1:end-1, :) - xz_force;
+    
+    position = position + velocity .* diff(time(1:2));
+    velocity = velocity + force .* diff(time(1:2));
+    
+    x_tmp = position(:, :, :, 1); x_tmp = x_tmp(:);
+    y_tmp = position(:, :, :, 2); y_tmp = y_tmp(:);
+    z_tmp = position(:, :, :, 3); z_tmp = z_tmp(:);
+    scatp.XData = x_tmp;
+    scatp.YData = y_tmp;
+    scatp.ZData = z_tmp;
+    drawnow
+    
+end
 
 
 
