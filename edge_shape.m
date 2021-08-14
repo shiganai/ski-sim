4ce17d7ba83418141f3b118fbfbbc424b0c8ce1f
shiganai <shiganai.hiro@gmail.com>
2021-08-14 14:13:05 +0900

    Big!!! update edge-shape.m
    Modify equations for edge_in_xyz.

--- edge_shape.m
+++ edge_shape.m
@@ -23,58 +23,60 @@ R = [
     ];
 
 Center_in_xyz = R * ([xCenter_on_alpha; yCenter_on_alpha; 0]) + 0.1 * [0; -sin(alpha); cos(alpha)];
-opposite_edge_in_xyz = Center_in_xyz + (Center_in_xyz - [xCenter_on_alpha; func_length; 0]);
+edge_in_xyz = [xCenter_on_alpha; yCenter_on_alpha ./ cos(alpha)];
+opposite_edge_in_xyz = Center_in_xyz + (Center_in_xyz - [edge_in_xyz; 0]);
 
 Center_in_xyz = Center_in_xyz';
+edge_in_xyz = edge_in_xyz';
 opposite_edge_in_xyz = opposite_edge_in_xyz';
 
 yCenter_on_alpha = matlabFunction(yCenter_on_alpha);
 xCenter_on_alpha = matlabFunction(xCenter_on_alpha);
 
 Center_in_xyz_fcn = matlabFunction(Center_in_xyz);
-func_length = matlabFunction(func_length);
+edge_in_xyz_fcn = matlabFunction(edge_in_xyz);
 opposite_edge_in_xyz_fcn = matlabFunction(opposite_edge_in_xyz);
 
-alpha_all = (16:-4:4)/32 * pi; alpha_all = alpha_all';
+% alpha_all = [4]/32 * pi; alpha_all = alpha_all';
+alpha_all = [16, 12, 8, 4, 3]/32 * pi; alpha_all = alpha_all';
 l = -1:1e-2:1; l = l';
-
-figure
-tiledlayout(2,size(alpha_all, 1))
 
 for alpha_index = 1:size(alpha_all, 1)
     
     alpha = alpha_all(alpha_index);
     
     Center_in_xyz = Center_in_xyz_fcn(alpha, l);
+    edge_in_xyz = [edge_in_xyz_fcn(alpha, l), zeros(size(l))];
     opposite_edge_in_xyz = opposite_edge_in_xyz_fcn(alpha, l);
     
-    nexttile
+    subplot(2, size(alpha_all, 1), alpha_index)
     scatter3(Center_in_xyz(:, 1), Center_in_xyz(:, 2), Center_in_xyz(:, 3))
     daspect([1, 1, 1])
     
+    title(['$$\alpha =', blanks(1), num2str(rad2deg(alpha)), '^\circ$$'], 'Interpreter', 'latex')
     xlabel('x')
     ylabel('y')
     zlabel('z')
     view([1, 0.1, 0.1])
     
     hold on
-    scatter3(xCenter_on_alpha(alpha, l), func_length(l), zeros(size(l)))
+    scatter3(edge_in_xyz(:, 1), edge_in_xyz(:, 2), edge_in_xyz(:, 3))
     scatter3(opposite_edge_in_xyz(:, 1), opposite_edge_in_xyz(:, 2), opposite_edge_in_xyz(:, 3))
     hold off
     
     hold on
-    plot3([opposite_edge_in_xyz(:, 1)'; xCenter_on_alpha(alpha, l)'], ...
-        [opposite_edge_in_xyz(:, 2)'; func_length(l)'], ...
-        [opposite_edge_in_xyz(:, 3)'; zeros(size(l))'], ...
+    plot3([opposite_edge_in_xyz(:, 1)'; edge_in_xyz(:, 1)'], ...
+        [opposite_edge_in_xyz(:, 2)'; edge_in_xyz(:, 2)'], ...
+        [opposite_edge_in_xyz(:, 3)'; edge_in_xyz(:, 3)'], ...
         'Color', 'black')
     hold off
     
-    curvature_d = fnder(spline(xCenter_on_alpha(alpha, l), func_length(l)));
+    curvature_d = fnder(spline(edge_in_xyz(:, 1), edge_in_xyz(:, 2)));
     curvature_dd = fnder(curvature_d);
     
-    curvature = fnval(curvature_dd, xCenter_on_alpha(alpha, l)) ./ (1 + fnval(curvature_d, xCenter_on_alpha(alpha, l)).^2).^(3/2);
+    curvature = fnval(curvature_dd, edge_in_xyz(:, 1)) ./ (1 + fnval(curvature_d, edge_in_xyz(:, 1)).^2).^(3/2);
     
-    nexttile
+    subplot(2, size(alpha_all, 1), alpha_index + size(alpha_all, 1))
     plot(xCenter_on_alpha(alpha, l), curvature)
     ax = gca;
     ax.FontSize = 20;
