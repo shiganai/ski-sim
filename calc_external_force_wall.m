ea2ba92f43a8adb645bc0056eb2577aa9cf1f45b
shiganai <shiganai.hiro@gmail.com>
2021-08-14 20:56:50 +0900

    Create calc_external_force_wall.m

---
+++ calc_external_force_wall.m
@@ -0,0 +1,21 @@
+function external_force = calc_external_force_wall(position, limitation, mode, buffer, power)
+
+    
+position_y = position(:, :, :, 2);
+
+external_force = zeros(size(position));
+external_force_y = external_force(:, :, :, 2);
+
+if isequal(mode, 'smaller')
+    external_force_y_after = min(cat(4, external_force_y, -(exp((position_y - limitation) * buffer) - 1) * power), [], 4);
+elseif isequal(mode, 'bigger')
+    external_force_y_after = max(cat(4, external_force_y, (exp(-(position_y - limitation) * buffer) - 1) * power), [], 4);
+else
+    error('select mode; smaller or bigger')
+end
+
+external_force(:, :, :, 2) = external_force_y_after;
+
+
+end
+
